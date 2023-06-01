9154f63740683c901c30681fa8ec0a205cc3df2c
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-01 15:29:47 +0800

    test: add tes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,19 @@
-from main import count_pupa
 from typing import Dict
 from fastapi.testclient import TestClient
 from main import app
+from model_predict.predict import load_model
 
+# API TEST
 client = TestClient(app)
 def test_count_pupa():
     response = client.get("/predict")
     assert response.status_code == 200, "Status code not 200"
     assert isinstance(response.json(), Dict), "Response is not json body"
+
+
+# UNIT TEST
+
+def test_load_model():
+
+    model = load_model(model_path="/")
+    assert isinstance(model, object)
