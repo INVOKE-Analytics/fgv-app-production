7a75b59b7f3be7a06955623676ee0797a7dd2c39
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-08 10:41:42 +0800

    feat: update whole predict.py

--- model_predict/predict.py
+++ model_predict/predict.py
@@ -1,7 +1,29 @@
-import torch 
+from pathlib import Path
+from yolov5.detect import run
 
 
-def load_model(model_path:str):
+def predict_pupa():
+    IMG_SIZE = (1024, 1024)
+    CONF_THRESHOLD: float = 0.20
+    model_output_path = Path(
+        "/home/ammar/INVOKE/fgv_API/fgv_app_production/model_output"
+    )
+    MODEL_PATH: str = model_output_path / "best_100.pt"
+    img_path: str = model_output_path / "ulat_test.png"
+    result_path = str(model_output_path)
+    output = run(
+        weights=MODEL_PATH,
+        imgsz=IMG_SIZE,
+        conf_thres=CONF_THRESHOLD,
+        source=img_path,
+        project=result_path,
+        save_txt=True,
+        save_conf=True,
+        save_crop=True,
+    )
 
-    uploaded_model = torch.load(model_path)
-    return uploaded_model
+    return output
+
+
+if __name__ == "__main__":
+    predict_pupa()
