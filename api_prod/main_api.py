292724b40da64ecf2b7a16c5fec3974c628cb6ef
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-08 15:15:16 +0800

    created main_api

---
+++ api_prod/main_api.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, Depends, UploadFile, File, HTTPException, status
+from typing import Annotated
+from model_predict.predict import PredictPupa
+import sys, threading
+from PIL import Image
+import io
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"Health test": "Good!"}
+
+
+@app.post("/predict")
+async def predict_metisa(pupa_img: UploadFile = File(...)):
+    try:
+        content = pupa_img.file.read()
+        with open(pupa_img.filename, "wb") as f:
+            f.write(content)
+        # read_img = pupa_img.file.read()
+        # with open(pupa_img.filename, "rb") as f:
+        #     img_bytes = f.read()
+        # content = Image.open(io.BytesIO(img_bytes))
+
+    except Exception as readError:
+        raise {"readError message": readError}
+
+    finally:
+        f.close()
+
+    try:
+        pred = PredictPupa(img_path=pupa_img.filename)
+        output = pred.predict_pupa()
+        output.save()
+        output.crop()
+    except Exception as errorRecog:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Error: {errorRecog}",
+        )
+    return {
+        "filename": pupa_img.filename,
+        "content-type": pupa_img.content_type,
+        "image-bbox": output.pandas().xyxy[0].to_json(orient="records"),
+    }
