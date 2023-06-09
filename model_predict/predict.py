58f25e8f8e480b054fdbeb460a8360dbab7cce3a
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-09 11:03:32 +0800

    refactor path,, result save at infrence_out dir

--- model_predict/predict.py
+++ model_predict/predict.py
@@ -7,24 +7,13 @@ class PredictPupa:
     def __init__(self, img_path):
         self.IMG_SIZE = (1024, 1024)
         self.CONF_THRESHOLD: float = 0.20
-        self.model_output_path = Path("../fgv_app_production/model_output")
-        self.MODEL_WEIGHT_PATH: str = self.model_output_path / "best_100.pt"
-        # img_path: str = model_output_path / "ulat_test.png"
+        self.project_path = Path("../fgv_app_production")
+        self.model_output_path = self.project_path / "model_output"
+        self.MODEL_WEIGHT_PATH: str = self.project_path / "model_output/best_100.pt"
         self.result_path = str(self.model_output_path)
         self.img_path = img_path
 
     def predict_pupa(self):
-        # output = run(
-        #     weights=self.MODEL_WEIGHT_PATH,
-        #     imgsz=self.IMG_SIZE,
-        #     conf_thres=self.CONF_THRESHOLD,
-        #     source=self.img_path,
-        #     project=self.result_path,
-        #     save_txt=True,
-        #     save_conf=True,
-        #     save_crop=True,
-        # )
-
         model = torch.hub.load(
             repo_or_dir="../fgv_app_production/yolov5",
             model="custom",
@@ -32,6 +21,7 @@ class PredictPupa:
             path=self.MODEL_WEIGHT_PATH,
         )
         result = model(self.img_path)
+        result.save(save_dir="../fgv_app_production/inference_out/", exist_ok=True)
         return result
 
 
